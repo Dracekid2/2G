a069fa24b1cc8719489f763b54ce3d70f50bc294
Dracekid2 <stepan.klumpler@gnj.cz>
2025-03-27 18:11:55 +0100

    Delete kalkulačka.py

--- kalkulačka.py
+++
@@ -1,9 +0,0 @@
-print("Zadej dvě čísla a já ti je sečtu")
-
-# Získání čísel od uživatele
-x = int(input("Zde zadejte nějaké číslo: "))
-y = int(input("Zde zadejte druhé číslo: "))
-
-# Sečtení a výpis výsledku
-součet = x + y
-print("Součet čísel je:", součet)
