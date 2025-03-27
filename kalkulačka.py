f1229452a49638c87548d92b8f08a706de37e962
Dracekid2 <stepan.klumpler@gnj.cz>
2025-03-27 17:46:15 +0100

    Add files via upload

---
+++ kalkulačka.py
@@ -0,0 +1,9 @@
+print("Napiš dvě čísla a já ti je sečtu")
+
+# Získání čísel od uživatele
+x = int(input("Zde zadejte nějaké číslo: "))
+y = int(input("Zde zadejte druhé číslo: "))
+
+# Sečtení a výpis výsledku
+součet = x + y
+print("Součet čísel je:", součet)
